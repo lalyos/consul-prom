6f5cebb6d2c7428113a7611777741e6f8011f569
deathowl <bcsergo@hortonworks.com>
2016-11-15 16:43:29 +0100

    entrypointery

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sleep 10
+/usr/bin/find /opt/prometheus/data/ -type f -name LOCK -delete
+rm -f /opt/prometheus/data/DIRTY
+/usr/local/bin/prometheus -config.file=/etc/prometheus/prometheus.yml -web.external-url=https://$PUBLIC_IP/prom/ -storage.local.path=/opt/prometheus/data &
+sleep 5
+/usr/local/bin/consul-template \
+       -consul consul.service.consul:8500 \
+       -template <(echo '{{key "etc/prometheus/config.yml"}}'):/etc/prometheus/prometheus.yml:"kill -HUP $(pidof prometheus)" \
+       -template <(echo '{{key "etc/prometheus/rules/all-rules.rule"}}'):etc/prommetheus/rules/all-rules.rule:"kill -HUP $(pidof prometheus)" \
+       -template <(echo '{{key "etc/prometheus/sdiscovery/node_collector.yml"}}' ):/etc/prometheus/sdiscovery/node_collector.yml:"kill -HUP $(pidof prometheus)" \
+       -template <(echo '{{key "etc/prometheus/sdiscovery/jmx_exporter.yml"}}' ):/etc/prometheus/sdiscovery/jmx_exporter.yml:"kill -HUP $(pidof prometheus)"
