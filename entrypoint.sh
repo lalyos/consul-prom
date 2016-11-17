2b1b5dc22184f6c4e6f14ce05fa846f9ee7968fc
lalyos <lalyos@yahoo.com>
2016-11-17 10:18:33 +0100

    fix sh to bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ rm -f /opt/prometheus/data/DIRTY
 /usr/local/bin/prometheus -config.file=/etc/prometheus/prometheus.yml -web.external-url=https://$PUBLIC_IP/prom/ -storage.local.path=/opt/prometheus/data &
 sleep 5
 
-curl consul.service.consul:8500/v1/kv/prometheus-entrypoint.sh | jq .[].Value -r | base64 -d | sh
+curl consul.service.consul:8500/v1/kv/prometheus-entrypoint.sh | jq .[].Value -r | base64 -d | bash -x
 
 : << KOMMENT
 /usr/local/bin/consul-template \
@@ -15,5 +15,5 @@ curl consul.service.consul:8500/v1/kv/prometheus-entrypoint.sh | jq .[].Value -r
        -template <(echo '{{key "etc/prometheus/config.yml"}}'):/etc/prometheus/prometheus.yml:"kill -HUP $(pidof prometheus)" \
        -template <(echo '{{key "etc/prometheus/rules/all-rules.rule"}}'):/etc/prometheus/rules/all-rules.rule:"kill -HUP $(pidof prometheus)" \
        -template <(echo '{{key "etc/prometheus/sdiscovery/node_collector.yml"}}' ):/etc/prometheus/sdiscovery/node_collector.yml:"kill -HUP $(pidof prometheus)" \
-       -template <(echo '{{key "etc/prometheus/sdiscovery/jmx_exporter.yml"}}' ):/etc/prometheus/sdiscovery/jmx_exporter.yml:"kill -HUP $(pidof prometheus)"
+       -template <(echo '{{key "etc/prometheus/sdiscovery/jmx_resourcemanager.yml"}}' ):/etc/prometheus/sdiscovery/jmx_resourcemanager.yml:"kill -HUP $(pidof prometheus)"
 KOMMENT
